af0436f7136b8c5285d8085e4de854fa392aaa03
Epharus Ndumia <epharusndumia@gmail.com>
2023-06-04 08:52:55 +0300

    added review methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,19 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def leave_review(user, star_rating, comment)
+      reviews.create(user: user, star_rating: star_rating, comment: comment)
+    end
+  
+    def print_all_reviews
+      reviews.each do |review|
+        review.print_review
+      end
+    end
+  
+    def average_rating
+      reviews.average(:star_rating).to_f
+    end
   end
   
