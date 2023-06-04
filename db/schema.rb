c6c092c64fdc271f746503c88bb0e47b0d39685e
Epharus Ndumia <epharusndumia@gmail.com>
2023-06-04 06:56:20 +0300

    created user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_04_023943) do
+ActiveRecord::Schema.define(version: 2023_06_04_035120) do
 
   create_table "reviews", force: :cascade do |t|
     t.integer "star_rating"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2023_06_04_023943) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["product_id"], name: "index_reviews_on_product_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
